387b27c0343f3b19579b98505b83280ff6aa9c8d
GH-Zhou <zhou.gu@husky.neu.edu>
2017-05-12 22:02:45 -0400

    comment lecture codes in server.js

--- server.js
+++ server.js
@@ -8,9 +8,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // configure a public directory to host static content
 app.use(express.static(__dirname + '/public'));
 
-
-var ourApp = require ("./lectures/angular/app.js");
-ourApp(app);
+// var ourApp = require ("./lectures/angular/app.js");
+// ourApp(app);
 
 require ("./test/app.js")(app);
 
