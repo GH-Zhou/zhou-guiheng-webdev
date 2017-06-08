b7ebdf4583c0a512f4b2a2655a3f247a4c8a1553
GH-Zhou <zhou.gu@husky.neu.edu>
2017-06-07 21:01:29 -0400

    delete unnecessary part in flightStatus.service.client

--- public/project/flightStatus.service.client.js
+++ public/project/flightStatus.service.client.js
@@ -16,26 +16,6 @@
                     authorization:"Bearer " + bearer_token
                 }
             }).then(function(response) {
-                console.log(response);
-                var attributes = {};
-
-                var departureFrom = response.data.FlightStatusResource.Flights.Flight.Departure.AirportCode;
-                var departureTimeLocal = response.data.FlightStatusResource.Flights.Flight.Departure.ActualTimeLocal.DateTime;
-                var departureTimeStatus = response.data.FlightStatusResource.Flights.Flight.Departure.TimeStatus.Definition;
-                var gateDeparture = response.data.FlightStatusResource.Flights.Flight.Departure.Terminal.Gate + ", T" +
-                                    response.data.FlightStatusResource.Flights.Flight.Departure.Terminal.Name;
-
-                var arrivalAt = response.data.FlightStatusResource.Flights.Flight.Arrival.AirportCode;
-                var arrivalTimeLocal = response.data.FlightStatusResource.Flights.Flight.Arrival.ActualTimeLocal.DateTime;
-                var arrivalTimeStatus = response.data.FlightStatusResource.Flights.Flight.Arrival.TimeStatus.Definition;
-                var gateArrival = response.data.FlightStatusResource.Flights.Flight.Arrival.Terminal.Gate + ", T" +
-                                  response.data.FlightStatusResource.Flights.Flight.Arrival.Terminal.Name;
-
-                var operatingFlightNumber = response.data.FlightStatusResource.Flights.Flight.OperatingCarrier.AirlineID +
-                                            response.data.FlightStatusResource.Flights.Flight.OperatingCarrier.FlightNumber;
-                var aircraftCode = response.data.FlightStatusResource.Flights.Flight.Equipment.AircraftCode;
-                var flightStatus = response.data.FlightStatusResource.Flights.Flight.FlightStatus.Definition;
-
                 return response.data.FlightStatusResource.Flights.Flight;
             });
         }
