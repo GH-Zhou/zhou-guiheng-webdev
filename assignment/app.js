dc6b16ffa6040656945aaf1595b4957d50d2c784
GH-Zhou <zhou.gu@husky.neu.edu>
2017-06-12 00:08:02 -0400

    update online connection

--- assignment/app.js
+++ assignment/app.js
@@ -1,6 +1,15 @@
 var mongoose = require('mongoose');
 mongoose.Promise = require('q').Promise;
-mongoose.connect('mongodb://localhost/zhou-guiheng-webdev');
+
+var connectionString = 'mongodb://localhost/zhou-guiheng-webdev'; // for local
+if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
+    var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
+    var password = process.env.MLAB_PASSWORD_WEBDEV;
+    connectionString = 'mongodb://' + username + ':' + password;
+    connectionString += '@ds139665.mlab.com:39665/heroku_kn1vs4tc'; // user yours
+}
+
+mongoose.connect(connectionString);
 
 require('./services/user.service.server');
 require('./services/website.service.server');
